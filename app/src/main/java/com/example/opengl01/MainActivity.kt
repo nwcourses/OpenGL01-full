95afa8c36e25d18c355dd5d8552ceaf0dd57b148
Nick Whitelegg <nickw4426@gmail.com>
2022-11-22 20:40:16 +0100

    up to wk9 also square with vertex indices

--- app/src/main/java/com/example/opengl01/MainActivity.kt
+++ app/src/main/java/com/example/opengl01/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.opengl01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val openGLView = findViewById<OpenGLView>(R.id.glview)
+
+        findViewById<Button>(R.id.plusX).setOnClickListener {
+            openGLView.renderer.translateCamera(1f, 0f, 0f)
+        }
+        findViewById<Button>(R.id.minusX).setOnClickListener {
+            openGLView.renderer.translateCamera(-1f, 0f, 0f)
+        }
+        findViewById<Button>(R.id.plusY).setOnClickListener {
+            openGLView.renderer.translateCamera(0f, 1f, 0f)
+        }
+        findViewById<Button>(R.id.minusY).setOnClickListener {
+            openGLView.renderer.translateCamera(0f, -1f, 0f)
+        }
+        findViewById<Button>(R.id.plusZ).setOnClickListener {
+            openGLView.renderer.translateCamera(0f, 0f, 1f)
+        }
+        findViewById<Button>(R.id.minusZ).setOnClickListener {
+            openGLView.renderer.translateCamera(0f, 0f, -1f)
+        }
+        findViewById<Button>(R.id.rotateClockwise).setOnClickListener {
+            openGLView.renderer.rotateCamera(-10f)
+        }
+        findViewById<Button>(R.id.rotateAnticlockwise).setOnClickListener {
+            openGLView.renderer.rotateCamera(10f)
+        }
+        findViewById<Button>(R.id.cameraForward).setOnClickListener {
+            openGLView.renderer.moveCamera(1.0f)
+        }
+        findViewById<Button>(R.id.cameraBack).setOnClickListener {
+            openGLView.renderer.moveCamera(-1.0f)
+        }
     }
 }
